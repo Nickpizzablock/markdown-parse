5535146bc5ed1ea13542531258e04873ea155245
Nickjoshuablock <61505392+Nickjoshuablock@users.noreply.github.com>
2022-01-20 12:21:11 -0800

    Passed!
    It passes now

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,16 +10,23 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            //System.out.println(currentIndex);
-        }
-        return toReturn;
+        
+            while(currentIndex < markdown.length()) {
+                int nextOpenBracket = markdown.indexOf("[", currentIndex);
+                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+                int openParen = markdown.indexOf("(", nextCloseBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                //toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+                //System.out.println(currentIndex);
+                if (nextOpenBracket == -1) {
+                    break;
+                }
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
+            return toReturn;
+        
     }
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
